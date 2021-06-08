102fb6c6a096a2d4c25449a6ac63ac26922e5659
miftahhadi817 <ajago6028@gmail.com>
2021-06-08 07:15:12 +0100

    Output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -1,13 +1,23 @@
 public class DataTypes{
  public static void main(String[]args){
-  String namaDepan = "Mif";
-  String namaTengah = "Tah";
+  String namaDepan = "Miftah";
+  String namaTengah = "Hadi";
   String namaBelakang = "Hadi";
 
-  int usia;
-  int lamaKuliah;
+  int usia = 20;
+  int lamaKuliah = 4;
   double ipk;
   char nilai = 'A';
+
+  System.out.println("Nama depan : ");
+  System.out.println(namaDepan);
+  System.out.println("Nama tengah : " + namaTengah);
+  System.out.println("Usia : " + usia);
+  System.out.println("Usia saat lulus (String): " + usia + lamaKuliah);
+  System.out.println("Usia saat lulus (Kurung): " + (usia + lamaKuliah));
+
+  int usiaLulus = usia + lamaKuliah;
+  System.out.println("Usia saat lulus (Variabel): " + usiaLulus);  
 
  }
 }
