71a025370a49cb6ce19ef229d07421b20771548e
miftahhadi817 <ajago6028@gmail.com>
2021-06-08 06:35:42 +0100

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[]args){
+  String namaDepan = "Mif";
+  String namaTengah = "Tah";
+  String namaBelakang = "Hadi";
+
+  int usia = 18;
+  int lamaKuliah = 4;
+  double ipk = 3.89764512;
+  char nilai = 'A';
+  bolean tampan = true;  
+
+ }
+}
